102ea4a0808cdb6068a5e2c5b03a5b76dd1164d4
prathikshakamathk <prathiksha.kamath@niveussolutions.com>
2023-03-18 16:06:27 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "app.terraform.io/chrip-cert-org/network/google"
     version = "3.4.0"
 
-    project_id   = "var.project"
+    project_id   = var.project
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
